b1458488ade40cb47ed5dc9b92db4fb227dc49cf
thinh <thinhqn999@gmail.com>
2025-03-30 23:17:05 +0700

    chia product thành displayproduct để giảm độ phức tạp của code

---
+++ WebBanHang/Pages/DisplayProductsBase.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+using WebBanHang.Models.Dtos;
+using WebBanHang.Services.Contracts;
+
+namespace WebBanHang.Pages
+{
+  public class DisplayProductsBase : ComponentBase
+  {
+    //[Inject]
+    //public IProductService ProductService { get; set; }
+
+    [Parameter]
+    public IEnumerable<ProductDto> Products { get; set; }
+
+    //protected override async Task OnInitializedAsync()
+    //{
+    //  Products = await ProductService.GetItems();
+    //}
+  }
+}
